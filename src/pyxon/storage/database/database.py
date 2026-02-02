2b38c7fe6879dd78377c9703662517a174448aa5
Ahmad Alqaisi <ahmad.alqaisi215@gmail.com>
2026-02-02 22:38:25 +0300

    🐛 (database): instantiate Settings to correctly read DATABASE_URL

--- src/pyxon/storage/database/database.py
+++ src/pyxon/storage/database/database.py
@@ -1,10 +1,12 @@
+# src.pyxon.storage.database.database
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from src.config import Settings
 
-SQLALCHEMY_DATABASE_URL = Settings.DATABASE_URL
+SQLALCHEMY_DATABASE_URL = Settings().DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
